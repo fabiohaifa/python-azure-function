87cc35b6b938d1b14bceff1e530afba3179f9ee7
Fabio Padua <fabiopadua@microsoft.com>
2024-03-08 12:05:36 -0300

    feat: first commit on this azure function project

---
+++ function-a/__init__.py
@@ -0,0 +1,8 @@
+# This is Function1/__init__.py
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    # Your code for Function1 goes here
+    return func.HttpResponse("Function A completed")
